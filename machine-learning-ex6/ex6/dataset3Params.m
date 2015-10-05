bb6aa69b270860429348536c4bd22609e76bf13b
Copperbutton <buptzxk2012@gmail.com>
2015-10-04 22:50:56 -0700

    Finished exercise 6

---
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -0,0 +1,63 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%EX6PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = EX6PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+% Candidate parameters.
+can = [0.01; 0.03; 0.1; 0.3; 1; 3; 10; 30];
+m = size(can, 1);
+
+% Initiate min error: prediciotn = ~ yval, which means all prediction are run.
+min_error = mean(double(~yval ~= yval));
+%fprintf('%f', min_error);
+
+x1 = [1 2 1]; 
+x2 = [0 4 -1];
+
+% Iterate over candidate choices
+for i = 1:m
+  for j = 1:m
+    % Use a new model to train the model
+    model= svmTrain(X, y, can(i, 1), @(x1, x2) gaussianKernel(x1, x2, can(j, 1)));
+
+    % Make prediction with trained model on validation set and get the error rate.
+    predictions = svmPredict(model, Xval);
+    error = mean(double(predictions ~= yval));
+
+    % compare the error rate with min_error, find the optimal parameter.
+    if (error < min_error)
+      C = can(i, 1);
+      sigma = can(j, 1);
+      min_error = error;
+    end
+   end
+end
+
+
+
+
+
+
+% =========================================================================
+
+end
